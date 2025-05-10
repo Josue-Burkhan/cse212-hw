087d68b4c8f19391f1f44dad5144d2f78d75421d
joshua.burkhan <bai21030@byui.edu>
2025-05-10 14:30:06 -0500

    week1 teach file done

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -9,5 +9,19 @@ public class Program
         // one of your projects.
 
         Console.WriteLine("Hello Sandbox World!");
+
+
+
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        numbers.Insert(2, 322);
+        Console.WriteLine(numbers[2]);
+        Console.WriteLine($"The Count is:{numbers.Count}");
+
+        //xD
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
+
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -7,10 +7,32 @@ public static class ArraySelector
         var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+
+        var a1 = new[] {1, 2, 3, 4};
+        var a2 = new[] {10, 20, 30, 40};
+        var selectArray = new[] {1, 1, 2, 2, 1, 1, 2, 2};
+        var resultArray = ListSelector(a1, a2, selectArray);
+        Console.WriteLine("<int[]>{" + string.Join(", ", resultArray) + "}"); // {1, 2, 10, 20, 3, 4, 30, 40}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        List <int> result = new List<int>();
+        int i = 0, ii = 0;
+
+        foreach (int s in select)
+        {
+            if (s == 1)
+            {
+                result.Add(list1[i]);
+                i++;
+            }
+            else if (s == 2)
+            {
+                result.Add(list2[ii]);
+                ii++;
+            }
+        }
+        return result.ToArray();
     }
 }
